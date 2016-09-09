d0a377225fe2b93c278a2be017430944022692a1
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2016-09-09 11:24:24 +0200

    Add bare test classes

---
+++ assignment2-deliverable/src/test/java/no/uio/inf5750/assignment2/dao/StudentDAOTest.java
@@ -0,0 +1,7 @@
+package no.uio.inf5750.assignment2.dao;
+
+/**
+ * Created by pjurasek on 09.09.16.
+ */
+public class StudentDAOTest {
+}
---
+++ assignment2-deliverable/src/test/java/no/uio/inf5750/assignment2/service/StudentSystemTest.java
@@ -0,0 +1,7 @@
+package no.uio.inf5750.assignment2.service;
+
+/**
+ * Created by pjurasek on 09.09.16.
+ */
+public class StudentSystemTest {
+}
