3a38d08a81f57949b2600b8d4857b147bc58c8c2
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2016-09-09 18:26:06 +0200

    Implement DAO functionalities

--- assignment2-deliverable/src/main/java/no/uio/inf5750/assignment2/dao/hibernate/HibernateCourseDAO.java
+++ assignment2-deliverable/src/main/java/no/uio/inf5750/assignment2/dao/hibernate/HibernateCourseDAO.java
@@ -2,13 +2,19 @@ package no.uio.inf5750.assignment2.dao.hibernate;
 
 import no.uio.inf5750.assignment2.dao.CourseDAO;
 import no.uio.inf5750.assignment2.model.Course;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
  * Created by pjurasek on 09.09.16.
  */
+@Transactional
 public class HibernateCourseDAO implements CourseDAO {
 
     private SessionFactory sessionFactory;
@@ -19,31 +25,79 @@ public class HibernateCourseDAO implements CourseDAO {
 
     @Override
     public int saveCourse(Course course) {
-        return 0;
+        return (Integer) sessionFactory.getCurrentSession().save(course);
     }
 
     @Override
     public Course getCourse(int id) {
-        return null;
+        return (Course) sessionFactory.getCurrentSession().get(Course.class, id);
     }
 
     @Override
     public Course getCourseByCourseCode(String courseCode) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+
+        Course course = null;
+
+        try {
+            course = (Course) session.createQuery("FROM Course WHERE name = :code ORDER by id DESC")
+                    .setParameter("code", courseCode)
+                    .getSingleResult();
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+
+        return course;
     }
 
     @Override
     public Course getCourseByName(String name) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+
+        Course course = null;
+
+        try {
+            course = (Course) session.createQuery("FROM Course WHERE name = :name ORDER by id DESC")
+                    .setParameter("name", name)
+                    .getSingleResult();
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+
+        return course;
     }
 
     @Override
     public Collection<Course> getAllCourses() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+
+        Collection<Course> students = new ArrayList<Course>();
+
+        try {
+            students = (Collection<Course>) session.createQuery("FROM Course ORDER by id DESC").list();
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+
+        return students;
     }
 
     @Override
     public void delCourse(Course course) {
+        Session session = sessionFactory.getCurrentSession();
 
+        try {
+            session.delete(course);
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private EntityManager getEntityManager() {
+        return sessionFactory.createEntityManager();
     }
 }
--- assignment2-deliverable/src/main/java/no/uio/inf5750/assignment2/dao/hibernate/HibernateStudentDAO.java
+++ assignment2-deliverable/src/main/java/no/uio/inf5750/assignment2/dao/hibernate/HibernateStudentDAO.java
@@ -2,13 +2,16 @@ package no.uio.inf5750.assignment2.dao.hibernate;
 
 import no.uio.inf5750.assignment2.dao.StudentDAO;
 import no.uio.inf5750.assignment2.model.Student;
-import org.hibernate.SessionFactory;
+import org.hibernate.*;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
  * Created by pjurasek on 09.09.16.
  */
+@Transactional
 public class HibernateStudentDAO implements StudentDAO {
 
     private SessionFactory sessionFactory;
@@ -19,26 +22,59 @@ public class HibernateStudentDAO implements StudentDAO {
 
     @Override
     public int saveStudent(Student student) {
-        return 0;
+        return (Integer) sessionFactory.getCurrentSession().save(student);
     }
 
     @Override
     public Student getStudent(int id) {
-        return null;
+        return (Student) sessionFactory.getCurrentSession().get(Student.class, id);
     }
 
     @Override
     public Student getStudentByName(String name) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+
+        Student student = null;
+
+        try {
+            student = (Student) session.createQuery("FROM Student WHERE name = :name ORDER by id DESC")
+                    .setParameter("name", name)
+                    .getSingleResult();
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+
+        return student;
+
     }
 
     @Override
     public Collection<Student> getAllStudents() {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+
+        Collection<Student> students = new ArrayList<Student>();
+
+        try {
+            students = (Collection<Student>) session.createQuery("FROM Student ORDER by id DESC").list();
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+
+        return students;
     }
 
     @Override
     public void delStudent(Student student) {
 
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.delete(student);
+
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 }
